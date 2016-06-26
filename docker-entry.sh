da3fd45fc6ad475bb61810a16d867d6fdaa2bef8
František Dvořák <valtri@civ.zcu.cz>
2016-06-26 21:21:08 +0200

    Inherit docker entry script.

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,7 +1,5 @@
 #! /bin/bash -e
 
-service apache2 start || service apache2 start
-
 service mysql start
 if grep -q 'jdbcPassword.*changeit' /var/opt/midpoint/config.xml; then
   echo 'Setting a new password for midPoint DB'
@@ -11,6 +9,4 @@ if grep -q 'jdbcPassword.*changeit' /var/opt/midpoint/config.xml; then
   xmlstarlet ed --inplace --update '/configuration/midpoint/repository/jdbcPassword' --value "${pass}" /var/opt/midpoint/config.xml
 fi
 
-service tomcat8 start || :
-
-exec "$@"
+. /docker-entry-base.sh
